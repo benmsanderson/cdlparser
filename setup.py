f2736fa9453321252ed198f208a618929e92c119
Benjamin Sanderson <sanderson@carbon.local>
2022-10-10 21:47:31 +0200

    v23

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ import setuptools
 
 setuptools.setup(
     name="CDLparser",
-    version="0.22",
+    version="0.23",
     author="Ben Sanderson",
-    description="A python netcdf CDF parser",
+    description="A python3 netcdf CDL parser",
     packages=["cdlparser"]
     )
